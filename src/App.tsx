e8fcd138c0e5fd5d9d0f0a5f3da060647e9b8426
Ebernn <r1.bernier@orange.fr>
2025-03-11 00:22:55 +0100

    💄 Improved demo, styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import type { Component } from "solid-js";
-
 import styles from "./App.module.css";
-import SimulationCanvas from "./simulation/Simulation";
+import SimulationCanvas from "./simulation/SimulationCanvas";
 
-const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      {/* <SimulationCanvas tapeSize={16} dimensions={256} /> */}
-      <SimulationCanvas tapeSize={36} dimensions={170} />
-      {/* <SimulationCanvas tapeSize={64} dimensions={128} /> */}
-      {/* <SimulationCanvas tapeSize={144} dimensions={85} /> */}
-      {/* <SimulationCanvas tapeSize={256} dimensions={64} /> */}
-    </div>
-  );
-};
+const App: Component = () => (
+  <div class={styles.App}>
+    <SimulationCanvas tapeSize={4} dimensions={256} />
+    {/* <SimulationCanvas tapeSize={6} dimensions={170} /> */}
+    {/* <SimulationCanvas tapeSize={8} dimensions={128} /> */}
+    {/* <SimulationCanvas tapeSize={12} dimensions={85} /> */}
+    {/* <SimulationCanvas tapeSize={16} dimensions={64} /> */}
+  </div>
+);
 
 export default App;
